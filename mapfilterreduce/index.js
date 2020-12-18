d4eda72f7e707e6efcbe68e0063f75004aa4d472
Marquinho Filho <marcofilho978@gmail.com>
2020-12-17 22:55:15 -0300

    Projetos DIO

---
+++ mapfilterreduce/index.js
@@ -0,0 +1,26 @@
+const pets = [
+{
+    name:'rex',
+    type:'dog',
+    age:10
+},
+{
+    name:'miau',
+    type:'cat',
+    age:2
+},
+{
+    name:'gulp',
+    type:'fish',
+    age:1
+}
+]
+
+const newPets=pets.filter((pet)=>{
+    return pet.age < 5
+
+
+})
+
+console.log(pets)
+console.log(newPets)
---
+++ primeirapagina/js/main.js
@@ -0,0 +1,141 @@
+
+
+
+
+
+/*
+Desafio
+Precisamos saber quanto uma determinada empresa deve pagar para seus colaboradores, porém temos apenas a quantidade de horas trabalhadas e o valor hora. Escreva um programa que leia o número de um colaborador, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse colaborador. Em seguida, apresente o número e o salário do colaborador, com duas casas decimais.
+
+Entrada
+Você receverá 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada.
+
+Saída
+Exiba o número e o salário do colaborador, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+
+ 
+Exemplos de Entrada	Exemplos de Saída
+25
+100
+5.50
+
+NUMBER = 25
+SALARY = U$ 550.00
+
+1
+200
+20.50
+
+NUMBER = 1
+SALARY = U$ 4100.00
+
+6
+145
+15.55
+
+NUMBER = 6
+SALARY = U$ 2254.75
+
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);
+*/
+
+
+
+/*function passar(elemento){
+
+    elemento.innerHTML="Obrigado caralho";
+    //alert("Oi caralho");
+    //document.getElementById("mousemove").innerHTML="Obrigado caralho";
+}
+
+function voltar(elemento){
+    elemento.innerHTML="Tchau caralho";
+    //document.getElementById("mousemove").innerHTML="Tchau caralho";
+}
+
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML="VLW";
+}
+
+function redirecionar(){
+    window.open("https://www.netflix.com");
+    //window.location.href="https://www.netflix.com";
+}
+
+function load(){
+    alert("Carregado");
+
+}
+
+function funcaochange(elemento){
+    console.log(elemento.value)  
+}*/
+/*var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+
+/*var count;
+for(count=0; count <=5; count++){
+    console.log(count);
+}*/
+
+
+
+/*var count = 0;
+while(count <=5){
+    console.log(count);
+    alert(count);
+    count ++;
+}*/
+
+
+
+/*var idade= prompt("Qual sua idade:");
+if (idade >=18){
+    alert("Maior de idade");
+}else{
+alert("Menor de idade");
+
+};*/
+
+/*var frutas = [{nome:"maça", cor:"Vermelha"}, {nome:"uva ", cor:"Roxo"}]
+console.log(frutas.nome);
+alert(frutas[1].cor);
+*/
+
+//var lista=["maça","pêra","laranja"];
+
+
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//console.log(lista.length); quantidade de elementos na lista
+//console.log(lista.reverse()); lista reversa
+//lista.pop(); tirar da array
+//lista.push("uva"); colocar na array
+//console.log(lista[1]);
+//console.log(lista);
+//alert(lista[1]);
+
+
+//var name="Marco Alberto";
+//var idade = 20;
+//var idade2 = 10;
+//var frase= "Japão é o melhor time do mundo";
+//alert("Bem Vindo " + name);
+//alert(idade + idade2);
+//console.log(name);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase("Japão", "Brasil"));
+//console.log(frase.toLowerCase("Japão", "Brasil"));
